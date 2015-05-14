1f05f27466081f9aea4b58c03320d9d08f06d7ac
Inku <365008398@qq.com>
2015-05-14 10:38:51 +0800

    commit

---
+++ src/main/java/com/ifeng/cms/servlet/ServletConfigDemo.java
@@ -0,0 +1,32 @@
+package com.ifeng.cms.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+/**
+ * Copyright ©Inku All Rights Reserved.
+ * Created by zhengpeng on 15/4/13.
+ */
+public class ServletConfigDemo extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+
+        ServletConfig config = this.getServletConfig();
+        Enumeration<String> en = config.getInitParameterNames();
+        while (en.hasMoreElements()) {
+            String name = en.nextElement();
+            String value = config.getInitParameter(name);
+            out.println(name + "-->" + value);
+        }
+    }
+}
